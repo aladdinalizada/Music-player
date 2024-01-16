cb70f3537b0df6661c25d232453e807ef0aa3142
aladdinalizada <tu7c5zneb@code.edu.az>
2024-01-16 23:45:45 +0400

    complete Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import PlayerControls from "./components/PlayerControls";
 import TrackList from "./components/TrackList";
+import Header from "./layout/Header";
 
 function App() {
   return (
     <>
       <div>
+        <Header />
+
         <TrackList />
         <PlayerControls />
       </div>
---
+++ src/layout/Header/index.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="text">
+      <h1>Music Player</h1>
+    </header>
+  );
+};
+
+export default Header;
